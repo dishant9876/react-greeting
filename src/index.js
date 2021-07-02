d7a37001be48343156998f20b1b37f67884b8211
Dishant Wadhwani <wadhwaniyash1234@gmail.com>
2021-07-02 21:33:01 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// var React = require('react');
+import React from 'react';
+// var ReactDOM = require('react-dom');
+import ReactDom from 'react-dom';
+import App from './App';
+
+
+ReactDom.render(
+    <App /> , document.getElementById('root')
+  );
